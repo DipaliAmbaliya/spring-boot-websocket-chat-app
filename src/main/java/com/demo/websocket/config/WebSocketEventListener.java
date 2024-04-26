55f865f56d9df4c2a8227ca17c5ef92092e2e2eb
dipali.ambaliya <simform.dipali.ambaliya@carsaver.com>
2024-04-26 18:58:43 +0530

    First commit

--- src/main/java/com/demo/websocket/config/WebSocketEventListener.java
+++ src/main/java/com/demo/websocket/config/WebSocketEventListener.java
@@ -1,13 +1,14 @@
-package com.alibou.websocket.config;
+package com.demo.websocket.config;
 
-import com.alibou.websocket.chat.ChatMessage;
-import com.alibou.websocket.chat.MessageType;
+import com.demo.websocket.chat.ChatMessage;
+import com.demo.websocket.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
@@ -16,7 +17,10 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 public class WebSocketEventListener {
 
     private final SimpMessageSendingOperations messagingTemplate;
-
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        System.out.println("Received a new web socket connection");
+    }
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
@@ -30,5 +34,4 @@ public class WebSocketEventListener {
             messagingTemplate.convertAndSend("/topic/public", chatMessage);
         }
     }
-
 }
